050b1e016ade834869fe6063d76ef61a931fb1f9
Michael Winkworth <mwinkwo@btinternet.com>
2024-01-27 18:15:48 +0000

    Install prettier

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 import { clientId, guildId } from './config.json';
 
@@ -7,22 +6,25 @@ import { config } from 'dotenv';
 config();
 
 const commands = [
-    new SlashCommandBuilder().setName('image')
+    new SlashCommandBuilder()
+        .setName('image')
         .setDescription('Fetches a random image from Google Images of a query')
-        .addStringOption((option) => option.setName('query')
-            .setDescription('The query to search Google Images for')
-            .setRequired(true)),
-    new SlashCommandBuilder().setName('enableautoimages')
+        .addStringOption((option) =>
+            option.setName('query').setDescription('The query to search Google Images for').setRequired(true),
+        ),
+    new SlashCommandBuilder()
+        .setName('enableautoimages')
         .setDescription('Enables automatic images in the current channel'),
-    new SlashCommandBuilder().setName('disableautoimages')
+    new SlashCommandBuilder()
+        .setName('disableautoimages')
         .setDescription('Disables automatic images in the current channel'),
-    new SlashCommandBuilder().setName('8ball')
-        .setDescription('Get the magic 8-ball\'s response to the question')
-        .addStringOption((option) => option.setName('question')
-            .setDescription('The question to ask the magic 8-ball')
-            .setRequired(true))
-]
-    .map((command) => command.toJSON());
+    new SlashCommandBuilder()
+        .setName('8ball')
+        .setDescription("Get the magic 8-ball's response to the question")
+        .addStringOption((option) =>
+            option.setName('question').setDescription('The question to ask the magic 8-ball').setRequired(true),
+        ),
+].map((command) => command.toJSON());
 
 if (process.env.TOKEN == null || !process.env.TOKEN) {
     console.log('Token not found.');
@@ -31,9 +33,11 @@ if (process.env.TOKEN == null || !process.env.TOKEN) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-void (async(): Promise<void> => {
+void (async (): Promise<void> => {
     try {
-        await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: commands,
+        });
         console.log('Successfully registered application commands.');
     } catch (error) {
         console.error(error);
--- image-bot.ts
+++ image-bot.ts
@@ -1,5 +1,14 @@
 // The discord.js API
-import { type CommandInteraction, type Message, type BaseMessageOptions, type TextBasedChannel, Client, Events, GatewayIntentBits, Partials } from 'discord.js';
+import {
+    type CommandInteraction,
+    type Message,
+    type BaseMessageOptions,
+    type TextBasedChannel,
+    Client,
+    Events,
+    GatewayIntentBits,
+    Partials,
+} from 'discord.js';
 // The google-images API
 import GoogleImages from 'google-images';
 // The winston API
@@ -11,7 +20,7 @@ config();
 
 const loggerFormat = format.combine(
     format.timestamp(),
-    format.printf((info) => `${info.timestamp as string} ${info.level}: ${info.message as string}`)
+    format.printf((info) => `${info.timestamp as string} ${info.level}: ${info.message as string}`),
 );
 
 // Sets up the logger
@@ -19,32 +28,30 @@ const logger = createLogger({
     format: loggerFormat,
     transports: [
         new transports.Console({
-            format: format.combine(
-                format.colorize(),
-                loggerFormat
-            )
+            format: format.combine(format.colorize(), loggerFormat),
         }),
-        new transports.File({ filename: 'image-bot.log' })
-    ]
+        new transports.File({ filename: 'image-bot.log' }),
+    ],
 });
 
-if ((process.env.CSE_ID == null || !process.env.CSE_ID)
-    || (process.env.API_KEY == null || !process.env.API_KEY)
-    || (process.env.TOKEN == null || !process.env.TOKEN)) {
-    logger.error('One or more of the required API keys were not found. Have you set the correct environment variables?');
+if (
+    process.env.CSE_ID == null ||
+    !process.env.CSE_ID ||
+    process.env.API_KEY == null ||
+    !process.env.API_KEY ||
+    process.env.TOKEN == null ||
+    !process.env.TOKEN
+) {
+    logger.error(
+        'One or more of the required API keys were not found. Have you set the correct environment variables?',
+    );
     process.exit(1);
 }
 
 // The discord client used to interact with Discord
 const client = new Client({
-    intents: [
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.DirectMessages
-    ],
-    partials: [
-        Partials.Channel,
-        Partials.Message
-    ]
+    intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
+    partials: [Partials.Channel, Partials.Message],
 });
 
 // The google images client used to search for images
@@ -65,7 +72,7 @@ const ballResponses = [
     'absolutely not',
     'stupid question',
     'nodding',
-    'shaking my head'
+    'shaking my head',
 ];
 
 // Returns a random number between a minimum and a maximum
@@ -85,25 +92,28 @@ function getSafeSetting(channel: TextBasedChannel): 'high' | 'off' {
     let nsfw = true;
 
     if ('nsfw' in channel) {
-        // eslint-disable-next-line prefer-destructuring
         nsfw = channel.nsfw;
     }
 
     return nsfw ? 'off' : 'high';
 }
 
-// eslint-disable-next-line max-params,max-len
-async function postRandomImage<T>(query: string, sendMessage: (message: BaseMessageOptions | string) => Promise<T>, channel: TextBasedChannel | null, gifsOnly = false): Promise<T> {
+async function postRandomImage<T>(
+    query: string,
+    sendMessage: (message: BaseMessageOptions | string) => Promise<T>,
+    channel: TextBasedChannel | null,
+    gifsOnly = false,
+): Promise<T> {
     // Get the current request time
     const newTime = new Date().getTime() - lastRequestTime.getTime();
 
     // Check the messages are not being sent too quickly
     if (newTime <= 2500) {
-        return sendMessage('Don\'t spam so fast, I\'m on cooldown.');
+        return sendMessage("Don't spam so fast, I'm on cooldown.");
     }
 
     if (query.includes('child')) {
-        return sendMessage('No thanks, I don\'t feel like getting on a watchlist.');
+        return sendMessage("No thanks, I don't feel like getting on a watchlist.");
     }
 
     // Get a random page number. A page is a section of 10 images.
@@ -116,7 +126,10 @@ async function postRandomImage<T>(query: string, sendMessage: (message: BaseMess
 
     // Query the image client for an image
     try {
-        const images = await imageClient.search(query, { page: pageNumber, safe: safeSetting });
+        const images = await imageClient.search(query, {
+            page: pageNumber,
+            safe: safeSetting,
+        });
 
         if (images.length > 0) {
             let image = { url: '' };
@@ -125,8 +138,13 @@ async function postRandomImage<T>(query: string, sendMessage: (message: BaseMess
             const maxCount = 10;
 
             // Check if the image has a suitable file extension
-            // eslint-disable-next-line no-unmodified-loop-condition
-            while (!(((image.url.endsWith('.jpg') || image.url.endsWith('.png')) && !gifsOnly) || image.url.endsWith('.gif')) && count <= maxCount) {
+            while (
+                !(
+                    ((image.url.endsWith('.jpg') || image.url.endsWith('.png')) && !gifsOnly) ||
+                    image.url.endsWith('.gif')
+                ) &&
+                count <= maxCount
+            ) {
                 image = images[random(0, images.length - 1)];
                 count += 1;
             }
@@ -151,29 +169,35 @@ async function postRandomImage<T>(query: string, sendMessage: (message: BaseMess
 }
 
 async function postRandomImageWithChannel(query: string, channel: TextBasedChannel): Promise<Message> {
-    return postRandomImage(query, async(message) => channel.send(message), channel);
+    return postRandomImage(query, async (message) => channel.send(message), channel);
 }
 
-async function postRandomImageWithInteraction(query: string, interaction: CommandInteraction, gifsOnly: boolean): Promise<void> {
-    await postRandomImage(query, async(message) => interaction.reply(message), interaction.channel, gifsOnly);
+async function postRandomImageWithInteraction(
+    query: string,
+    interaction: CommandInteraction,
+    gifsOnly: boolean,
+): Promise<void> {
+    await postRandomImage(query, async (message) => interaction.reply(message), interaction.channel, gifsOnly);
 }
 
 client.once(Events.ClientReady, () => {
     logger.info('I am ready!');
 });
 
-client.on(Events.MessageCreate, async(message) => {
+client.on(Events.MessageCreate, async (message) => {
     // Check that a human sent the message and that there is some content
     if (!message.author.bot && message.content.length > 0) {
         if (autoChannels.get(message.channel.id) ?? false) {
-            logger.info(`User '${message.author.username}' requested the image '${message.content}' as an automatic image`);
+            logger.info(
+                `User '${message.author.username}' requested the image '${message.content}' as an automatic image`,
+            );
 
             await postRandomImageWithChannel(message.content, message.channel);
         }
     }
 });
 
-client.on(Events.InteractionCreate, async(interaction) => {
+client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) {
         return;
     }
@@ -192,7 +216,9 @@ client.on(Events.InteractionCreate, async(interaction) => {
             if (!alreadyEnabled) {
                 autoChannels.set(channelId, true);
                 messageText = 'Automatic images have been enabled in this channel.';
-                logger.info(`Automatic images were enabled in channel: ${getChannelName(interaction.channel)}, id: ${channelId}`);
+                logger.info(
+                    `Automatic images were enabled in channel: ${getChannelName(interaction.channel)}, id: ${channelId}`,
+                );
             } else {
                 messageText = 'Automatic images are already enabled in this channel.';
             }
@@ -215,7 +241,9 @@ client.on(Events.InteractionCreate, async(interaction) => {
             if (!alreadyDisabled) {
                 autoChannels.set(channelId, false);
                 messageText = 'Automatic images have been disabled in this channel.';
-                logger.info(`Automatic images were disabled in channel: ${getChannelName(interaction.channel)}, id: ${channelId}`);
+                logger.info(
+                    `Automatic images were disabled in channel: ${getChannelName(interaction.channel)}, id: ${channelId}`,
+                );
             } else {
                 messageText = 'Automatic images are already disabled in this channel.';
             }
@@ -242,7 +270,9 @@ client.on(Events.InteractionCreate, async(interaction) => {
             const question = interaction.options.getString('question');
 
             if (question != null) {
-                logger.info(`User '${interaction.user.username}' wanted to know the magic 8-ball's answer to the following question: '${question}'`);
+                logger.info(
+                    `User '${interaction.user.username}' wanted to know the magic 8-ball's answer to the following question: '${question}'`,
+                );
 
                 const randomResponse = ballResponses[random(0, ballResponses.length - 1)];
 
@@ -250,7 +280,9 @@ client.on(Events.InteractionCreate, async(interaction) => {
 
                 await postRandomImageWithInteraction(`${randomResponse} gif`, interaction, true);
             } else {
-                await interaction.reply('The magic 8-ball does not understand what you want - you need to give it a question!');
+                await interaction.reply(
+                    'The magic 8-ball does not understand what you want - you need to give it a question!',
+                );
             }
 
             break;
